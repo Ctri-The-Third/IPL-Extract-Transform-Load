ec04c4300ed4e8c0e4476a5681bee33944dace4c
C'tri Goudie <43475390+Ctri-The-Third@users.noreply.github.com>
2019-06-06 12:05:01 +0100

    addition of unicode handling, and fixed handling of requestID to increment.

--- FetchUser.py
+++ FetchUser.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 import csv
-
+import sys
 
 
 def outputPlayerToConsole(json,idAsString):
@@ -27,8 +27,8 @@ def outputPlayerToConsole(json,idAsString):
                 str(json["centre"][i]["missions"]),
                 str(int(json["centre"][i]["skillLevelNum"])+1),
                 str(json["centre"][i]["skillLevelName"])
-            )
-            outputFile.write(outputStr)
+            ).encode(sys.stdout.encoding,errors='replace')
+            outputFile.write(outputStr.decode(outputFile.encoding))
             i = i + 1
         outputFile.close()
 
@@ -38,12 +38,14 @@ API_AchievementsURL = 'http://v2.iplaylaserforce.com/achievements.php'
 API_recentMissions = 'http://v2.iplaylaserforce.com/recentMissions.php'
 targetRegion = 7
 targetSite = 9
-targetMember = 186 #current start
+targetMember = 9839 #current start
 
+counter = 1
+print("======preparing to read and write.======")
+print(sys.stdout.encoding)
+while targetMember < 15000: 
 
-while targetMember < 30000: 
-
-    data = {'requestId':'1', 
+    data = {'requestId':str(counter), 
             'regionId':'9999', 
             'siteId':'9999', 
             'memberRegion':str(targetRegion),
@@ -54,14 +56,15 @@ while targetMember < 30000:
     #print("===============\nRead Complete, response = \n"+r.text+"\n===============")
     responseJSON = json.loads(r.text)
 
-    MemberIDAsString = "{0}-{1}-{2}".format(str(targetRegion),str(targetSite),str(targetMember))
+    MemberIDAsString = "{3}: {0}-{1}-{2}".format(str(targetRegion),str(targetSite),str(targetMember),str(counter))
 
     if 'centre'  in responseJSON and len(responseJSON['centre']) >= 1:
         outputPlayerToConsole(responseJSON,MemberIDAsString)
     else:
-        print ("Did not find player: {0}".format(MemberIDAsString))
+        print ("{0} was not found".format(MemberIDAsString))
 
     targetMember = targetMember + 1
+    counter = counter + 1
     time.sleep(1)
 
 
