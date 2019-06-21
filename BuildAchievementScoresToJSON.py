2a167b04765cfa967ace901cc8291be446671530
C'tri Goudie <43475390+Ctri-The-Third@users.noreply.github.com>
2019-06-21 14:23:22 +0100

    Exclude incomplete achievements from count
    Also includes a comparison query for quickly comparing to player IDs to find out who's rocking which achievements

--- BuildAchievementScoresToJSON.py
+++ BuildAchievementScoresToJSON.py
@@ -3,7 +3,7 @@ import json
 from SQLconnector import connectToSource
 
 SQL = '''
-select TOP (15) players.PlayerID, GamerTag, AchievementScore, count ( case when achievedDate is null then 0 when achievedDate is not null then 1 end) as AchievementsCompleted from players
+select TOP (15) players.PlayerID, GamerTag, AchievementScore, sum ( case when achievedDate is null then 0 when achievedDate is not null then 1 end) as AchievementsCompleted from players
 join PlayerAchievement on Players.PlayerID = PlayerAchievement.PlayerID
 group by players.PlayerID, GamerTag, AchievementScore
 order by AchievementScore desc
---
+++ SQL Queries/Achievements - Who's got which.sql
@@ -0,0 +1,17 @@
+
+with Player1Achievements as (
+select Image, PlayerID From PlayerAchievement 
+where achievedDate is not null
+and PlayerID = '9-6-106'),
+Player2Achievements as (
+select Image, PlayerID from PlayerAchievement
+where achievedDate is not null
+and PlayerID = '7-9-220040')
+
+select Player1Achievements.PlayerID, a.AchName , Player2Achievements.PlayerID, b.AchName from Player1Achievements full outer join Player2Achievements on
+Player1Achievements.Image = Player2Achievements.Image
+full outer join AllAchievements a on Player1Achievements.Image = a.image
+full outer join AllAchievements b on Player2Achievements.Image = b.image
+where (Player1Achievements.PlayerID is null 
+OR Player2Achievements.PlayerID is null )
+and not (Player1Achievements.PlayerID is null AND Player2Achievements.PlayerID is null)
