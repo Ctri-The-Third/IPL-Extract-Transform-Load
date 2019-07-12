b690028398f8dc0c180fb8385ae6b7a13e7f7abf
C'tri Goudie <43475390+Ctri-The-Third@users.noreply.github.com>
2019-07-12 17:12:14 +0100

    Added additional games to qualify for Head to Heads

--- SQL Queries/HeadToHeads.sql
+++ SQL Queries/HeadToHeads.sql
@@ -6,7 +6,7 @@ with data as (
 	join games g on p.GameUUID = g.GameUUID 
 	join players pl on pl.PlayerID = p.PlayerID
 
-	where GameName in ('Individual', 'Color Ranked', 'Highlander')
+	where GameName in ('Individual', 'Colour Ranked', 'Highlander','Individual Supercharge','Gladiator (Individual)','Shadows (Individual)')
 )
 	select top(7) d1.PlayerID, d1.Score, d1.GamerTag,  d2.PlayerID, d2.GamerTag, d2.Score,  d1.GameName, convert(varchar(20),d1.GameTimestamp,106) as GT, d1.gameMonth
 	from data d1 join data d2 on d1.GameTimestamp = d2.GameTimestamp and d1.PlayerID != d2.PlayerID and d1.Score >= d2.Score
