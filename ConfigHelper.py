85d14a36d6242d6b65a4bfcdc934c1e4a678a8b2
C'tri Goudie <43475390+Ctri-The-Third@users.noreply.github.com>
2019-08-13 17:56:10 +0100

    Can now switch between EDB and PBO blob generation

--- ConfigHelper.py
+++ ConfigHelper.py
@@ -7,3 +7,28 @@ def getConfig():
         config  = json.load(f)
 
     return config
+
+
+def setActive(index):
+    with open("AppConfig.json","r") as f:
+        config  = json.load(f)
+
+        foundConfigs = "configs" in config
+        if not foundConfigs:
+            return "Failed, didn't find config object in config"
+        foundConfigIndex = len(config["configs"]) >= (index + 1)
+        if not foundConfigIndex:
+            return "Failed, didn't find config %i" % (index)
+        
+        config["SiteNameReal"] = config["configs"][index]["SiteNameReal"]
+        config["SiteNameShort"] = config["configs"][index]["SiteNameShort"]
+        config["ID Prefix"] = config["configs"][index]["ID Prefix"]
+
+        f = open("AppConfig.json", "w+")
+        f.write(json.dumps(config,indent=2))
+        f.close()
+
+        return "Successfully set primary arena to [%s]" % (config["SiteNameReal"])
+
+def setNewDates(startDate,endDate):
+    return "not yet implemented"
