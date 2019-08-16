07dba8da8a4c06a64b11b10af92275176c5d3448
C'tri Goudie <43475390+Ctri-The-Third@users.noreply.github.com>
2019-08-16 10:01:05 +0100

    Achievement handling across multiple arenas

--- SQL Queries/Achievements - Edinburgh Ranking.sql
+++ SQL Queries/Achievements - Edinburgh Ranking.sql
@@ -1,4 +1,14 @@
-select players.PlayerID, GamerTag, AchievementScore, sum ( case when achievedDate is null then 0 when achievedDate is not null then 1 end) as AchievementsCompleted from players
+declare @arenaName as varchar(50)
+set @arenaName = ''
+
+select 
+	players.PlayerID, 
+	GamerTag, 
+	AchievementScore, 
+	AllAchievements.ArenaName,
+	sum ( case when achievedDate is null then 0 when achievedDate is not null then 1 end) as AchievementsCompleted 
+from players
 join PlayerAchievement on Players.PlayerID = PlayerAchievement.PlayerID
-group by players.PlayerID, GamerTag, AchievementScore
-order by AchievementScore desc
+join AllAchievements on PlayerAchievement.Image = AllAchievements.image
+group by players.PlayerID, GamerTag, AchievementScore, ArenaName
+order by PlayerID desc
