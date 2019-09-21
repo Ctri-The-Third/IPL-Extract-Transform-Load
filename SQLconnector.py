77340de35cc751e8e066a79563eb8b89f66bf596
C'tri <CtriCoding@Ctri.co.uk>
2019-09-21 21:17:15 +0100

    Changes to connection SQL file

--- SQLconnector.py
+++ SQLconnector.py
@@ -1,9 +1,13 @@
-import pyodbc
-import uuid
-
-
+#import pyodbc
+import psycopg2
 
 def connectToSource():
-
-    conn = pyodbc.connect('Driver={SQL Server}; Server=CTRI-DESKTOP\SQLEXPRESS; Database=LaserScraper; Trusted_Connection=yes;')
+    conn = psycopg2.connect(host='localhost',user='ctri', password='Prayer1')
+    #conn = pyodbc.connect('Driver={SQL Server}; Server=CTRI-DESKTOP\SQLEXPRESS; Database=LaserScraper; Trusted_Connection=yes;')
     return conn
+
+def execute():
+    result = {}
+    return result 
+
+connectToSource()
