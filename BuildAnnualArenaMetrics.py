b3ad88c21a725cb566190cabe175efe3ed05ed52
C'tri <CtriCoding@Ctri.co.uk>
2020-01-03 19:56:22 +0000

    Annual stats builders

---
+++ BuildAnnualArenaMetrics.py
@@ -0,0 +1,168 @@
+from SQLconnector import connectToSource
+import ConfigHelper
+import json
+conn = connectToSource()
+cursor = conn.cursor()
+
+outputObject = {}
+
+#topGames
+print ("  == Which games were played")
+
+SQL = """
+select count(*), gamename from games g 
+where arenaName ilike %s
+and gametimestamp >= %s
+and gametimestamp <= %s
+group by 2
+order by 1 desc;
+"""
+cfg = ConfigHelper.getConfig()
+
+parameters = (
+      cfg['SiteNameReal'], cfg['StartDate'], cfg['EndDate']
+      )
+conn = connectToSource()
+cursor = conn.cursor()
+
+cursor.execute(SQL,parameters)
+results = cursor.fetchall()
+SQLdesc = cursor.description
+descCount = 0
+
+for desc in SQLdesc:
+    #print("%s %s" % (descCount,desc[0]))
+    descCount = descCount + 1
+
+gamesPlayed = []
+for result in results:
+    print (result)
+    game = {'gameName' : result[1], 'timesPlayed' : result[0] }
+    gamesPlayed.append(game)
+outputObject['gamesPlayed'] = gamesPlayed
+#referrals
+print ("  == Referrals and Welcomers")
+
+SQL = """
+
+with firstGames as 
+(
+	select gamerTag, p.playerID, min(gametimestamp) as firstGame
+	from players pl 
+	join participation p on p.playerID = pl.playerID
+	join games g  on p.gameuuid = g.gameuuid
+	group by 2,1
+),
+playersInGames as 
+(
+	select count(*) as countOfPlayersInGame, gametimestamp
+	from players pl
+	join participation p on p.playerID = pl.playerID
+	join games g  on p.gameuuid = g.gameuuid
+	group by 2
+),
+gamesWithNewPlayers as 
+(
+	select 	count (case when gametimestamp = firstgame then 1 else null end) as newPlayers
+	,	count (case when gametimestamp != firstgame then 1 else null end) as existingPlayers
+	,	gametimestamp
+	,	arenaname
+	from players pl
+	join participation p on p.playerID = pl.playerID
+	join games g  on p.gameuuid = g.gameuuid
+	join firstGames fg on pl.playerID = fg.playerID
+	where gametimestamp >= %s /*StartDate*/ and gametimestamp < %s /*EndDate*/
+	and arenaname ilike %s /*SiteNameReal*/
+	group by 3,4
+	order by 3 desc
+),
+referredPlayers as 
+(
+	
+	select pl.gamertag, g.gametimestamp, firstgame, newplayers, existingplayers,
+	case when firstgame = g.gametimestamp then 'REFERRED!' else 'referee' end as referalRole
+	from players pl
+	join participation p on p.playerID = pl.playerID
+	join games g  on p.gameuuid = g.gameuuid
+	join firstGames fg on pl.playerID = fg.playerID
+	join gamesWithNewPlayers gwnp on gwnp.gametimestamp = g.gametimestamp and gwnp.arenaname = g.arenaname
+	where newplayers > 0 and existingPlayers > 0 
+	order by g.gametimestamp desc
+)
+select count(distinct gamerTag), 'new player' as playerRole from referredPlayers
+	where referalrole = 'REFERRED!'
+union 
+select count (distinct gamerTag), 'veteran' from referredPlayers
+	where referalrole = 'referee'
+"""
+
+
+parameters = (
+      cfg['StartDate'], cfg['EndDate'], cfg['SiteNameReal']
+      )
+conn = connectToSource()
+cursor = conn.cursor()
+
+cursor.execute(SQL,parameters)
+results = cursor.fetchall()
+SQLdesc = cursor.description
+descCount = 0
+for desc in SQLdesc:
+    #print("%s %s" % (descCount,desc[0]))
+    descCount = descCount + 1
+
+referrals = {"newPlayers" : results[0][0], "welcomers" : results[1][0]}
+outputObject['referrals'] = referrals
+for result in results:
+    print (result)
+
+#new and old players
+print ("  == New and Departed players")
+
+SQL = """
+with playerMetrics as (
+    select gamertag
+    , pl.playerID
+    , arenaname
+    , min(gametimestamp) as firstGame 
+    , max(gametimestamp) as lastGame 
+    from participation p 
+    join players pl on p.playerID = pl.playerID
+    join games g on p.gameuuid = g.gameuuid
+    where arenaName ilike %s --arenaName
+    group by gamerTag, pl.playerID, arenaName
+),
+playerMissions as (
+    select playerID, count(*) as gamesPlayedInPeriod from participation p  join games g on p.gameuuid = g.gameuuid
+    where gametimestamp >= %s --startDate
+    and gametimestamp < %s --endDate
+    group by 1
+)
+select 
+     count (case when gamesPlayedInPeriod > 0 then 1 else null end) as totalPlayingPlayers
+   , count (case when firstGame >= %s then 1 else null end) as newPlayers
+   , count (case when ((lastGame < to_date(%s,'YYYY-MM-DD') - INTERVAL '%s days' ) and lastGame >= '2019-06-01' ) then 1 else null end ) as churnedPlayers 
+from playerMetrics pm1 join playerMissions pm2 on pm1.playerID = pm2.playerID
+
+
+"""
+
+parameters = (
+    cfg['SiteNameReal'], cfg['StartDate'], cfg['EndDate']
+    , cfg['StartDate'],cfg['EndDate'], cfg['ChurnDuration']
+    )
+conn = connectToSource()
+cursor = conn.cursor()
+
+cursor.execute(SQL,parameters)
+results = cursor.fetchall()
+SQLdesc = cursor.description
+descCount = 0
+for desc in SQLdesc:
+    print("%s %s" % (descCount,desc[0]))
+    descCount = descCount + 1
+
+playerCounts = {'activePlayers' : results[0][0], 'newPlayers':results[0][1], 'churnedPlayers':results[0][2]}
+outputObject['playerCounts'] = playerCounts
+
+print(json.dumps(outputObject,indent=4))
--- SQL Queries/ArenaDemographics-allYear.sql
+++ SQL Queries/ArenaDemographics-allYear.sql
@@ -1,26 +1,28 @@
-with playerMetrics as (
-    select gamertag,arenaname,min(gametimestamp) as firstGame ,max(gametimestamp) as lastGame , count(*) 
+﻿with playerMetrics as (
+    select gamertag
+    , pl.playerID
+    , arenaname
+    , min(gametimestamp) as firstGame 
+    , max(gametimestamp) as lastGame 
     from participation p 
     join players pl on p.playerID = pl.playerID
     join games g on p.gameuuid = g.gameuuid
-    group by 1,2
-
-
+    where arenaName ilike '%edinburgh%'
+    group by gamerTag, pl.playerID, arenaName
 ),
 playerMissions as (
-    select playerID, count(*) from participation p  
+    select playerID, count(*) as gamesPlayedInPeriod from participation p  join games g on p.gameuuid = g.gameuuid
+    where gametimestamp >= '2019-06-01'
+    and gametimestamp < '2020-01-01'
+    group by 1
 )
 select 
---     count (*)
---   , count (case when firstGame > '2019-01-01' then 1 else null end) as newPlayers
---   , count (case when (firstGame < '2019-08-01' and lastGame < '2019-09-01' and lastgame > '2019-01-01') then 1 else null end ) as churnedPlayers
-* 
-from playerMetrics
-where firstgame < '2019-08-01'
-and lastGame < '2019-09-01'
-and lastGame > '2019-01-01'
+     count (case when gamesPlayedInPeriod > 0 then 1 else null end) as totalPlayingPlayers
+   , count (case when firstGame >= '2019-06-01' then 1 else null end) as newPlayers
+   , count (case when ((lastGame < to_date('2020-01-01','YYYY-MM-DD') - INTERVAL '60 days' ) and lastGame >= '2019-06-01' ) then 1 else null end ) as churnedPlayers 
+from playerMetrics pm1 join playerMissions pm2 on pm1.playerID = pm2.playerID
+
 
 --firstPlayed
 --lastPlayed
 --playerID
-
