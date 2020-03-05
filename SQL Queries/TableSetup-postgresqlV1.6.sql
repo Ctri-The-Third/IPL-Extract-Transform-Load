ce281e817d4ea3645db3ad04d1d966bad4fa5075
C'tri <CtriCoding@Ctri.co.uk>
2020-03-05 11:09:37 +0000

    New source of truth for Stars
    new view that calculates stars in the DB, rather than each point (big5,monthly, annual) calculating them individually.

--- SQL Queries/TableSetup-postgresqlV1.6.sql
+++ SQL Queries/TableSetup-postgresqlV1.6.sql
@@ -22,3 +22,48 @@ CREATE OR REPLACE VIEW public."interestingplayers"
     WHERE players.missions > 15 OR pas.level >= 4;
 
 ALTER TABLE AllAchievements ALTER COLUMN AchName TYPE varchar(100);
+
+
+-- View: public."participationWithStars"
+
+CREATE OR REPLACE VIEW public."participationWithStars" AS 
+ WITH data AS (
+         SELECT pl.playerid,
+            pl.gamertag,
+            g.gamename,
+            g.gametimestamp,
+            g.arenaname,
+            p.score,
+            row_number() OVER (PARTITION BY g.gametimestamp, g.arenaname ORDER BY g.gametimestamp DESC, p.score DESC) AS rank,
+            count(p.playerid) OVER (PARTITION BY g.gametimestamp, g.arenaname ORDER BY g.gametimestamp DESC) AS playercount,
+            to_char(g.gametimestamp, 'YYYY-MM'::text) AS gamemonth
+           FROM participation p
+             JOIN games g ON p.gameuuid::text = g.gameuuid::text
+             JOIN players pl ON p.playerid::text = pl.playerid::text
+        ), datawithstars AS (
+         SELECT data.playerid,
+            data.gamertag,
+            data.gamename,
+            data.gametimestamp,
+            data.arenaname,
+            data.score,
+            data.rank,
+            data.playercount,
+            data.gamemonth,
+            data.playercount * data.playercount / data.rank AS starsforgame
+           FROM data
+        )
+ SELECT datawithstars.playerid,
+    datawithstars.gamertag,
+    datawithstars.gamename,
+    datawithstars.gametimestamp,
+    datawithstars.arenaname,
+    datawithstars.score,
+    datawithstars.rank,
+    datawithstars.playercount,
+    datawithstars.gamemonth,
+    datawithstars.starsforgame
+   FROM datawithstars;
+
+ALTER TABLE public."participationWithStars"
+  OWNER TO "LaserScraper";
