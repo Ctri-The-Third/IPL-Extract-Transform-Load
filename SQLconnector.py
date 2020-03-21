f65bf205e8b8134a13244d2e142c90c56b7620f6
C'tri <CtriCoding@ctri.co.uk>
2020-03-21 19:37:05 +0000

    Connection string now derives from config

--- SQLconnector.py
+++ SQLconnector.py
@@ -1,16 +1,16 @@
 #import pyodbc
 import psycopg2
-
+import ConfigHelper
 counter = 0
 conn = None
 def connectToSource():
     global conn
     global counter
     
+    cfg = ConfigHelper.getConfig()
     
     if counter <= 0:
-
-        conn = psycopg2.connect(host='192.168.0.133',port=5432 ,user='LaserScraper', password='LaserScraper', database = 'LaserScraper')
+        conn = psycopg2.connect(host=cfg["DBConnectionString"]["host"],port=cfg["DBConnectionString"]["port"] ,user=cfg["DBConnectionString"]["username"], password=cfg["DBConnectionString"]["password"], database = cfg["DBConnectionString"]["database"])
         conn.set_session(autocommit = True)
     else:
         try:
