24a55c381c7f40e8ba9adffb6859efea3138bc4d
C'tri <CtriCoding@Ctri.co.uk>
2021-09-02 18:00:01 +0100

    Update FetchPlayerAndGames.py

--- FetchPlayerAndGames.py
+++ FetchPlayerAndGames.py
@@ -76,7 +76,7 @@ def queryIndividual(ID, scope = None):
         if playerNeedsUpdated == True or scope == "full":
             updatedPlayers.append(ID)
             missionsJson = fetchPlayerRecents_root('',region,site,IDPart)
-            if missionsJson != None:
+            if missionsJson != None and "mission" in missionsJson:
                 for mission in missionsJson["mission"]:
                 
                     missionUUID = addGame(mission[0],mission[1],mission[2])
